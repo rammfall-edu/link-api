acae25e7b1a03e44dc5a700d8b0b95dd52b90719
rammfall <15bogdan17@gmail.com>
2022-09-14 21:11:35 +0300

    Fix envs

--- src/initializers/db.mjs
+++ src/initializers/db.mjs
@@ -1,5 +1,16 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(
-  process.env.DB_CREDS || 'postgres://postgres:postgres@localhost:5556/postgres'
-);
+export const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  port: process.env.DB_PORT || 5556,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
+});
